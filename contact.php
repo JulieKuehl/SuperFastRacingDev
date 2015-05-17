ec80c2cf43d0781de4165502d369f2e208c9ff5a
Julie Kuehl <julie@juliekuehl.com>
2015-05-16 20:13:01 -0500

    layout on contact page
    this is bad practice that will need to be fixed later

--- contact.php
+++ contact.php
@@ -2,9 +2,12 @@
 
 		<h1>Contact SuperFast Racing</h1>
 		<form>
-			<label>Name:</label><input type="text" name="name"><br />
-			<label>Email:</label><input type="email" name="email"><br />
-			<label>Message:</label><textarea name="message"></textarea><br />
+			<label>Name:</label><input type="text" name="name"><br>
+			<br>
+			<label>Email:</label><input type="email" name="email"><br>
+			<br>
+			<label>Message:</label><textarea name="message"></textarea><br>
+			<br>
 			<input type="submit">
 		</form>
 		<img src="images/racetrack.png" alt="Clipart of cars on racetrack" title="Zoom zoom">
